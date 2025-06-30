4fcfa4943b3ef3e9b151b7494445d4017b5b3df3
Hafiz Muhammad Ajmal Naseer <88052202+enventorztech@users.noreply.github.com>
2025-06-30 13:22:50 +0500

    Update GoogleAuthService.cs

--- CentersBarCode/Services/GoogleAuthService.cs
+++ CentersBarCode/Services/GoogleAuthService.cs
@@ -16,7 +16,7 @@ namespace CentersBarCode.Services
     public class GoogleAuthService : IGoogleAuthService
     {
         // Client ID from your Google Developer Console - make sure this matches your OAuth configuration
-        private const string WebClientId = "613797922873-vmu2lko85b1mkv0lno9is9d557ggog4o.apps.googleusercontent.com";
+        private const string WebClientId = "61665556806-56m6slh77n3vbnbduic59i3h915a7cub.apps.googleusercontent.com";
         
         #if ANDROID
         // Sign-in client
